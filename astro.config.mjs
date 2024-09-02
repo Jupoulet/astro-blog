ea24f95d1540c17d58a44a7776d91b05b9216953
Jupoulet <julien.pierre.picard@gmail.com>
2024-09-02 14:24:28 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    vite: {
+        build: {
+            charset: 'utf-8',
+        }
+    },
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react(), tailwind()],
+    site: 'https://Jupoulet.github.io',
+    base: '/astro-blog',
 });
---
+++ src/services/notion.ts
@@ -0,0 +1,32 @@
+import { Client } from '@notionhq/client';
+import { NotionToMarkdown } from 'notion-to-md';
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+export const fetchArticles = async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const response = await notion.databases.query({
+    database_id: databaseId!,
+  });
+
+  // Extract necessary information from the response
+  const articles = response.results.map((page: any) => ({
+    id: page.id,
+    title: page.properties.article.title[0].text.content,
+    slug: page.properties.slug.rich_text[0].text.content,
+    created: page.properties.created.date.start,
+    status: page.properties.Status.status.name,
+    url: page.url,
+  }));
+
+  return articles;
+};
+
+export const fetchPageContent = async (pageId: string) => {
+  // Convert Notion blocks to Markdown
+  const mdBlocks = await n2m.pageToMarkdown(pageId);
+  const markdown = n2m.toMarkdownString(mdBlocks);
+
+  return markdown.parent;
+};
