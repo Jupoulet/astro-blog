551352ce7c058504fbe124a9e3d1b841ab7feb2e
Jupoulet <julien.pierre.picard@gmail.com>
2024-09-03 12:26:11 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
-
-// Load dotenv only in development
-if (process.env.NODE_ENV === 'development') {
-    const dotenv = await import('dotenv');
-    dotenv.config();
-}
 
 // https://astro.build/config
 export default defineConfig({
