1c27c41c08d7685ad4906a382f8eba65367c0c1b
Jupoulet <julien.pierre.picard@gmail.com>
2024-09-03 12:24:35 +0200

    deploy with Fly.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
+
+// Load dotenv only in development
+if (process.env.NODE_ENV === 'development') {
+    const dotenv = await import('dotenv');
+    dotenv.config();
+}
 
 // https://astro.build/config
 export default defineConfig({
